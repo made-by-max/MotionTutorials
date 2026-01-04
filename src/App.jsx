80dda48ad004c6a207cd9a138cca83b8d5b8892e
made-by-max <madebymax.dev@gmail.com>
2026-01-04 13:36:41 -0800

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,7 @@ import FollowCursor from "./components/FollowCursor";
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
-      <Drag />
-=======
       <FollowCursor />
->>>>>>> 191073141166f9983a1b53c32b321b63c0fbf5f8
     </div>
   );
 }
